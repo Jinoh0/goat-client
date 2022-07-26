09d4632ca2256bc1afbded5aaaab2c622da12167
ykztenoks <danielskonetzky7@gmail.com>
2022-07-26 11:21:12 -0300

    created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/homePage/index";
+import { Login } from "./pages/login";
+import { EditProfile } from "./pages/editProfile";
+import { Profile } from "./pages/profile";
+import { UserProfile } from "./pages/userProfile";
+import { Question } from "./pages/question";
 import "./index.css";
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<HomePage />} />
+      <Route path="/feed" element={<HomePage />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/edit-profile" element={<EditProfile />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/:profileId" element={<UserProfile />} />
+      <Route path="/question/:questionId" element={<Question />} />
     </Routes>
   );
 }
