413aac06d780d28bcdf9257368a6d3a28a84392b
Jino Hong <jinohong1993@gmail.com>
2022-07-28 18:57:06 -0300

    version check

--- src/components/navbar/navBar/index.js
+++ src/components/navbar/navBar/index.js
@@ -1,12 +1,12 @@
 import { UserCard } from "../userCard";
 import { SettingsButton } from "../settingsButton";
-
+import style from "./style.module.css";
 
 export function NavBar() {
   return (
-    <>
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
       <UserCard />
       <SettingsButton />
-    </>
+    </div>
   );
 }
